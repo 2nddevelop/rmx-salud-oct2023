f27517e9bb4b4ec3abf9059f84592af52626d256
Municipio 365 <igob247@gmail.com>
2023-11-21 07:27:02 -0400

    buttons at Centros, Especialidades and TiposCliente

--- salud_web/src/views/parametros/Especialidades.vue
+++ salud_web/src/views/parametros/Especialidades.vue
@@ -26,36 +26,19 @@
             <tr v-for="(r, index) in regs" :key="r.esp_id">
               <td width="3%" align="right">{{ index + 1 }}</td>
               <td>
-                <button 
-                    @click="editRegistro(r)" 
-                    class="bg-blue-500 hover-bg-blue-600 text-white font-bold py-2 px-4 m-1 rounded" 
+                <button
+                    @click="editRegistro(r)"
+                    class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 m-1 rounded"
                     title="Editar"
-                >                    
-                    <svg xmlns="http://www.w3.org/2000/svg" 
-                        viewBox="0 0 20 20" 
-                        fill="currentColor" 
-                        class="w-5 h-5"
-                    >
-                    <path 
-                        d="M2.695 14.763l-1.262 3.154a.5.5 0 00.65.65l3.155-1.262a4 4 0 001.343-.885L17.5 5.5a2.121 2.121 0 00-3-3L3.58 13.42a4 4 0 00-.885 1.343z"
-                    />
-                    </svg>
-                </button>
-                <button @click="deleteRegistro(r)" 
-                  class="bg-red-500 hover-bg-red-600 text-white font-bold py-2 px-4 m-1 rounded" 
-                  title="Eliminar">
-                  <svg 
-                    xmlns="http://www.w3.org/2000/svg" 
-                    viewBox="0 0 20 20" 
-                    fill="currentColor" 
-                    class="w-5 h-5"
                   >
-                  <path 
-                    fill-rule="evenodd" 
-                    d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
-                    clip-rule="evenodd"
-                  />
-                  </svg>
+                    <i class="fa-solid fa-pencil"></i>
+                  </button>
+                  <button
+                    @click="deleteRegistro(r)"
+                    class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 m-1 rounded"
+                    title="Eliminar"
+                  >
+                    <i class="fa-solid fa-trash"></i>
                 </button>
               </td>
               <td align="right">{{ r.esp_codigo }}</td>
@@ -114,8 +97,10 @@
   </template>
   
   <script>
-
   import especialidadesService from '../../services/especialidadesService';
+  
+  import '@fortawesome/fontawesome-free/css/all.css';
+
   export default {
     data() {
       return {
