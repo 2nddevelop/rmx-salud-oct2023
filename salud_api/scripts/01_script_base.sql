59f419c4cd58447fa810abb401fb64b41f8a0b00
Municipio 365 <igob247@gmail.com>
2023-12-10 09:23:01 -0400

    database makeover

--- salud_api/scripts/01_script_base.sql
+++ salud_api/scripts/01_script_base.sql
@@ -2,21 +2,6 @@
 
 CREATE EXTENSION pgcrypto;
 
-CREATE TABLE base_tipos_membresias (
-  tme_id serial PRIMARY KEY,
-  tme_codigo text NOT NULL,
-  tme_descripcion text NOT NULL,
-  tme_costo decimal NOT NULL,
-  tme_registrado timestamp NOT NULL DEFAULT now(),
-  tme_actualizado timestamp,
-  tme_usr_id text not NULL, 
-  tme_estado text NOT NULL DEFAULT 'A'
-);
-INSERT INTO base_tipos_membresias (tme_codigo, tme_descripcion, tme_costo, tme_usr_id, tme_estado) VALUES 
-  ('ORO', 'Membrecia Oro', 50, 11, 'A'),
-  ('PLATA', 'Membrecia Plata', 40, 11, 'A'),
-  ('BRONCE', 'Membrecia Bronce', 10, 11, 'A');
-
 
 CREATE TABLE base_origenes (
   ori_id serial PRIMARY KEY,
@@ -43,8 +28,7 @@ INSERT INTO base_origenes (ori_codigo, ori_descripcion, ori_usr_id, ori_estado) 
 
 CREATE TABLE base_usuarios (
   usr_id serial PRIMARY KEY,
-  usr_ori_id integer NOT NULL,
-  usr_tme_id integer NOT NULL,
+  usr_cnt_id integer NOT NULL, // centro de salud
   usr_nombres text NOT NULL,
   usr_primer_apellido text NOT NULL,
   usr_segundo_apellido text NOT null,
@@ -58,17 +42,45 @@ CREATE TABLE base_usuarios (
   usr_actualizado timestamp,
   usr_usr_id text NOT NULL, 
   usr_estado text NOT NULL DEFAULT 'A',
+  FOREIGN KEY (usr_ori_id) REFERENCES base_origenes(ori_id)
+);
+INSERT INTO base_usuarios (usr_ori_id, usr_tme_id, usr_nombres, 
+  usr_primer_apellido, usr_segundo_apellido, usr_direccion, usr_celular, usr_email, 
+  usr_clave, usr_lat, usr_lng, usr_usr_id, usr_estado) VALUES 
+  (4, 1, 'Admin', 'Admin', 'Admin', 'Av Villa El Carmen, Calle 11, No. 100', '5917089699', 'rome@gmail.com', 
+  crypt('123456', gen_salt('bf')), '-24,77777777', '-24,77777777', 11, 'A');
+
+
+CREATE TABLE base_roles (
+  rol_id serial PRIMARY KEY,
+  rol_codigo text NOT NULL,
+  rol_descripcion text NOT NULL,
+  rol_registrado timestamp NOT NULL DEFAULT now(),
+  rol_actualizado timestamp,
+  rol_usr_id text NOT NULL, 
+  rol_estado text NOT NULL DEFAULT 'A',
   FOREIGN KEY (usr_ori_id) REFERENCES base_origenes(ori_id),
   FOREIGN KEY (usr_tme_id) REFERENCES base_tipos_membresias(tme_id)
 );
-INSERT INTO base_usuarios (usr_ori_id, usr_tme_id, usr_nombres, 
-  usr_primer_apellido, usr_segundo_apellido, usr_direccion, 
-  usr_celular, usr_email, usr_clave, usr_lat, usr_lng, usr_usr_id, usr_estado) VALUES 
-  (4, 1, 'Admin', 'Admin', 'Admin', 'Av Villa El Carmen, Calle 11, No. 100',
-  '70712345', 'rome@gmail.com', crypt('123456', gen_salt('bf')), 
-  '-24,77777777', '-24,77777777', 11, 'A');
+INSERT INTO base_roles (rol_codigo, rol_descripcion, rol_usr_id, rol_estado) VALUES 
+  ('Admin', 'Admin', 11, 'A');
 
 
+CREATE TABLE base_usuarios_roles (
+  urol_id serial PRIMARY KEY,
+  urol_usr_id text NOT NULL,
+  urol_registrado timestamp NOT NULL DEFAULT now(),
+  urol_actualizado timestamp,
+  urol_usr_id text NOT NULL, 
+  urol_estado text NOT NULL DEFAULT 'A',
+  FOREIGN KEY (usr_ori_id) REFERENCES base_origenes(ori_id),
+  FOREIGN KEY (usr_tme_id) REFERENCES base_tipos_membresias(tme_id)
+);
+INSERT INTO base_roles (urol_usr_id, urol_usr_id, urol_estado) VALUES 
+  (1, 11, 'A');
+
+
+-- INFORMATIVOS
 CREATE TABLE base_avisos (
   avi_id serial PRIMARY KEY,
   avi_titulo text NOT NULL,
@@ -93,17 +105,4 @@ CREATE TABLE base_noticias (
 );
 INSERT INTO base_noticias (nt_titulo, nt_contenido, nt_usr_id, nt_estado) VALUES 
   ('Noticia 1', 'Contenido 1 contenido 1 contenido 1 contenido 1 contenido 1.', 11, 'A');
-
-
-  CREATE TABLE base_comisiones (
-    com_id serial PRIMARY KEY,
-    com_titulo text NOT NULL,
-    com_contenido text NOT NULL,
-    com_registrado timestamp NOT NULL DEFAULT now(),
-    com_actualizado timestamp,
-    com_usr_id text not NULL, 
-    com_estado text NOT NULL DEFAULT 'A'
-  );
-  INSERT INTO base_comisiones (com_titulo, com_contenido, com_usr_id, com_estado) VALUES 
-    ('Comisionista 1', 'Se requiere comisionista para entrega LPZ-Bermejo, mayor información cel 77712345.', 11, 'A');
 
