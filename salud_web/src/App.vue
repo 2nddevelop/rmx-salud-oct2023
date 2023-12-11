353629cd5610c55a3dc36c389ac20624bbf16f34
Municipio 365 <igob247@gmail.com>
2023-12-11 14:49:42 -0400

    clean code

--- salud_web/src/App.vue
+++ salud_web/src/App.vue
@@ -45,7 +45,7 @@ const menuItems = ref([
     { id: 42, label: 'Pantalla', link: '/fichasPantalla' },
   ]},
   { id: 5, label: 'Consulta Médica', children: [
-    { id: 51, label: 'Llama_Fichas', link: '/fichasAtencion' },
+    { id: 51, label: 'Llamada_Fichas', link: '/fichasAtencion' },
   //  { id: 52, label: 'Consulta_Externa', link: '/fichasConsulta' },
     { id: 53, label: 'Consulta_Externa', link: '/fichasConsultaB' },
   ]},
