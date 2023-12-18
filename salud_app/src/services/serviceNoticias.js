a11309e155b3951286c8d0b7edc2ac8327f66f2e
Municipio 365 <igob247@gmail.com>
2023-12-18 00:41:50 -0400

    routing api

--- salud_app/src/services/serviceNoticias.js
+++ salud_app/src/services/serviceNoticias.js
@@ -1,46 +1,26 @@
-// apiService.js
+// apiService.ts
+import { ref } from 'vue';
+import axios from 'axios';
+import { useStore } from 'vuex';
+
+import { API_URL } from '../config.js';
 
 export const useServiceNoticias = () => {
-
-  const dataNoticias = [
-    {
-        nt_id: 1,
-        nt_titulo: 'Noticia 1',
-        nt_contenido: 'Noticia 1 Noticia 1 Noticia 1 Noticia 1 Noticia 1 Noticia 1 Noticia 1 ',
-        nt_registrado: '2023-01-30 10:30',
-        nt_usr_id: 11,
-        nt_estado: 'A'
-    },
-    {
-        nt_id: 2,
-        nt_titulo: 'Noticia 2',
-        nt_contenido: 'Noticia 2 Noticia 2 Noticia 2 Noticia 2 Noticia 2 Noticia 2 ',
-        nt_registrado: '2023-01-30 10:30',
-        nt_usr_id: 11,
-        nt_estado: 'A'
-    },
-    {
-        nt_id: 3,
-        nt_titulo: 'Noticia 3',
-        nt_contenido: 'Noticia 3 Noticia 3 Noticia 3 Noticia 3 Noticia 3 Noticia 3 Noticia 3 Noticia 3 Noticia 3 Noticia 3 ',
-        nt_registrado: '2023-01-30 10:30',
-        nt_usr_id: 11,
-        nt_estado: 'A'
-    },
-    {
-        nt_id: 4,
-        nt_titulo: 'Noticia 4',
-        nt_contenido: 'Noticia 4 Noticia 4 Noticia 4 Noticia 4 ',
-        nt_registrado: '2023-01-30 10:30',
-        nt_usr_id: 11,
-        nt_estado: 'A'
-    }
-  ];
+  const store = useStore();
 
   const fetchData = async () => {
     try {
-      const data = dataNoticias;
-      return data;
+      const token = store.state.globalToken;
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const endpoint = '/noticias';
+      const response = await axios.get(`${API_URL}${endpoint}`, config);
+      return response.data;
     } catch (error) {
       console.error('Error fetching data:', error);
       throw error;
@@ -51,3 +31,4 @@ export const useServiceNoticias = () => {
     fetchData,
   };
 };
+
--- salud_web/src/config.js
+++ salud_web/src/config.js
@@ -1,3 +1,3 @@
 // config.js
-export const API_URL = 'http://localhost:3000/api';
+export const API_URL = 'https://test-salud-api.chuquisaca.gob.bo/api';
 export const MAX_RESULTS = 10;
