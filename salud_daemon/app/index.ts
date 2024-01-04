4f4f173e980773b996ff80f657605eeaeabd6a34
usuario <jorgecopa@gmail.com>
2024-01-04 18:50:58 -0400

    Daemon: servicios SICE y conexión a varias BD

--- salud_daemon/app/index.ts
+++ salud_daemon/app/index.ts
@@ -1,18 +1,15 @@
-// index.ts
 import express from 'express';
-import bodyParser from 'body-parser';
 import router from '../middleware/router';
-import swaggerUi from 'swagger-ui-express';
-import swaggerJSDoc from 'swagger-jsdoc';
 import cors from 'cors';
 
 import https from 'https';
 import fs from 'fs';
 
 const app = express();
+app.disable('x-powered-by')
 const port = 8888;
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors({
   origin: '*',
   allowedHeaders: '*',
@@ -20,12 +17,6 @@ app.use(cors({
   preflightContinue: true,
   optionsSuccessStatus: 204
 }));
-//app.use((req, res, next) => {
-//  res.header('Access-Control-Allow-Origin', '*');
-//  res.header('Access-Control-Allow-Headers', '*');
-//  res.header('Access-Control-Allow-Methods', '*');
-//  next();
-//});
 
 app.get('/', (req, res) => {
   res.sendFile('public/app.html', { root: '.' });
@@ -33,26 +24,11 @@ app.get('/', (req, res) => {
 
 app.use('/api', router);
 
-// documentación swagger
-const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API Documentation',
-      version: '1.0.0',
-    },
-  },
-  apis: ['**/*.ts'], // Especifica la ruta de tus archivos de ruta/controlador
-};
-const swaggerSpec = swaggerJSDoc(swaggerOptions);
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-
 https
   .createServer({
     key: fs.readFileSync(__dirname + '/../../certificados/privatekey.pem'),
     cert: fs.readFileSync(__dirname + '/../../certificados/certificate.pem')
   }, app)
   .listen(port, () => {
-    console.log(`Servidor escuchando en el puerto ${port}\nSwagger en https://localhost:${port}/api/docs/`);
+    console.log(`Server listening at https://localhost:${port}`);
   });
